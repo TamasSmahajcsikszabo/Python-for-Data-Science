2aa25d08ca0b4b06247d9a1ad28f31b0d39ca467
TamasSmahajcsikszabo <tamas.smahajcsikszabo@outlook.hu>
2020-01-08 21:26:05 +0100

    ufuncs added

--- chapter2_numpy_computation.py
+++ chapter2_numpy_computation.py
@@ -1,7 +1,6 @@
 import numpy as np
 np.random.seed(0)
 
-
 def compute_reciprocals(values):
     output = np.empty(len(values))
     for i in range(len(values)):
@@ -23,3 +22,31 @@ print("theta:", theta)
 print("sin(theta):", np.sin(theta))
 print("cos(theta):", np.cos(theta))
 print("tan(theta):", np.tan(theta))
+
+# exponentials
+x = np.linspace(1, 8, num = 1000)
+np.power(x, 3)
+
+# logarithms
+np.log(x)
+np.log2(x)
+np.log10(x)
+
+from scipy import special as s
+s.gamma(x)
+s.gammaln(x)
+s.erf(x)
+
+# use of the 'out' parameter
+# writing output of calulcations into memory location
+x = np.arange(5)
+y = np.empty(5)
+np.multiply(x, 10, out = y)
+z = np.zeros(10)
+np.power(2, x, out=z[::2])
+print(z)
+
+# aggregates
+# reduce
+np.add.reduce(x)
+np.add.accumulate(x) # stores intermediate steps
