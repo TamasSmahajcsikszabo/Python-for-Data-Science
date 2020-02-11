c2239d81b7d627d9dbf1b12109c4aafa653b01a3
tamas <tamas@localhost.localdomain>
2020-02-11 22:06:11 +0100

    update

--- LP_OO_iter.py
+++ LP_OO_iter.py
@@ -176,3 +176,15 @@ print(next(I), next(I), next(I))
 for x in skipper:
     for y in skipper:
         print(x + y, end=' ')
+
+# iter() with yield generator function
+
+
+class Squares:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def __iter__(self):
+        for value in range(self.start, self.stop + 1):
+            yield value**2
