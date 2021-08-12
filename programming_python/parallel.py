0fa5d88b8ffe0332c9a389fa50f96fed5b615204
tamas <tamas.smahajcsikszabo@outlook.hu>
2021-08-12 22:01:56 +0200

    chapter 5

--- programming_python/parallel.py
+++ programming_python/parallel.py
@@ -1,7 +1,23 @@
 # fork processes
+# ~ forking is like copying a program in memory and running it parallel
+# ~ the original is the parent process, while the others are the children
+# ~ all run independently children can change from and outlive parents
+
+from multiprocessing.pool import ThreadPool
+import threading
+import queue
 import _thread
 import os
 import time
+from mazes import Maze
+
+maze = Maze(
+    10,
+    10,
+    indicator="maze",
+    name="maze",
+    cellsize=3,
+    algorithm="sidewinder")
 
 
 def child():
@@ -40,6 +56,8 @@ print('Main process exiting.')
 
 
 # threads
+# shared global memory space with process
+# global variable interchargebility
 
 
 def child(pid):
@@ -67,6 +85,8 @@ def counter(myId, count):
 for i in range(5):
     _thread.start_new_thread(counter, (i, 5))
 
+# at forks, children live after parents, here not! So the main thread has
+# to wait for the rest to finish
 time.sleep(6)
 print('Main thread exiting.')
 
@@ -76,7 +96,6 @@ print('Main thread exiting.')
 # corruption
 # _thread achieves this with *locks*
 
-import _thread, time
 
 def counter(myId, count):
     for i in range(count):
@@ -84,6 +103,7 @@ def counter(myId, count):
         mutex.acquire()
         print('[%s] => %s' % (myId, i))
         mutex.release()
+
 
 # the lock ensures mutually exclusive access to the stdout stream
 # output never overlaps as two threads never execute print calls at the same
@@ -100,24 +120,29 @@ print('Main thread exiting')
 stdoutmutex = _thread.allocate_lock()
 exitmutexes = [_thread.allocate_lock() for i in range(10)]
 
+
 def counter(myId, count):
     for i in range(count):
         stdoutmutex.acquire()
         print('[%s] => %s' % (myId, i))
         stdoutmutex.release()
-    exitmutexes[myId].acquire() # a child takes a lock
+    exitmutexes[myId].acquire()  # a child takes a lock
+
 
 for i in range(10):
     _thread.start_new_thread(counter, (i, 100))
 
-for mutex in exitmutexes:
-    while not mutex.locked(): pass # whether all locks have been taken
+for mutex in range(len(exitmutexes)):
+    print('Mutex {0} locked status is {1}'.format(mutex, exitmutexes[mutex].locked()))
+    while not exitmutexes[mutex].locked():
+        pass  # whether all locks have been taken
 print('Main thread exiting.')
 
 # alternative with global list of shared integers
 
 stdoutmutex = _thread.allocate_lock()
 exitmutexes = [False] * 10
+
 
 def counter(myId, count):
     for i in range(count):
@@ -126,26 +151,29 @@ def counter(myId, count):
         stdoutmutex.release()
     exitmutexes[myId] = True
 
+
 for i in range(10):
     _thread.start_new_thread(counter, (i, 100))
 
 
-while False in exitmutexes: pass
+while False in exitmutexes:
+    pass
 print('Main thread exiting.')
 
-
-# the queue module
-import queue
+# alternative with 'with' statement
+# 'with': the lock's context manager will acquire the lock on the code block,
+# and will release it once run no matter what exceptions are met
 
 stdoutmutex = _thread.allocate_lock()
 numthreads = 5
 exitmutexes = [_thread.allocate_lock() for i in range(numthreads)]
 
-def counter(myId, count, mutex):
+
+def counter(myId, count, mutex): # shared object of stdoutmutex is passed in
     for i in range(count):
         time.sleep(1/(myId + 1))
         with mutex:
-            print('[%s] => %s' % (myId, i))
+            print('[%s] => %s' % (myId, i)) # no need to call release, 'with' does it
     exitmutexes[myId].acquire()
 
 for i in range(numthreads):
@@ -156,7 +184,6 @@ print('Main thread exiting.')
 
 
 # threading module
-import threading
 
 class myThread(threading.Thread):
     def __init__(self, myId, count, mutex):
@@ -165,7 +192,7 @@ class myThread(threading.Thread):
         self.count = count
         threading.Thread.__init__(self)
 
-    def run(self): # run() provides thread logic
+    def run(self):  # run() provides thread logic and the thread's action
         for i in range(self.count):
             with self.mutex:
                 print('[%s] => %s' % (self.myId, i))
@@ -179,11 +206,13 @@ for i in range(10):
     threads.append(thread)
 
 for thread in threads:
-    thread.join() # the join() method waits until the thread exits
+    thread.join()  # the join() method waits until the thread exits
 print('Main thread exiting')
 
-# example 2 for threading
 
+
+# example 2 for threading
+# 1. subclass case, prefereed for OOP and if per-thread information is needed
 def action(i):
     print(i ** 32)
 
@@ -192,13 +221,77 @@ class myThread(threading.Thread):
         self.i = i
         threading.Thread.__init__(self)
 
-    def run(self): # run() provides thread logic
+    def run(self):  # run() provides thread logic
         print(self.i ** 32)
-
 myThread(2).start()
 
+# 2. pass in action to the 'target' (default Thread class) argument, i.e.
+# without subclasses
+thread = threading.Thread(target=(lambda: action(2)))
+thread.start()
+
+# synchronizing access to shared objects
+# without synchronizing:
+def not_synched():
+
+    def adder():
+        global count
+        count = count + 1
+        time.sleep(0.5)
+        count = count + 1
+
+    threads = []
+    for i in range(100):
+        thread = threading.Thread(target = adder, args=())
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads: thread.join()
+    print(count)
+
+count = 0
+not_synched()
+# even statements such as assignments are not guaranteed to run to completion
+# by themselves, i.e. they are not atomic
+
+# apply thread locks to synchronize:
+
+def foo(word, number):
+    print(word * number)
+    return number
 
 
+words = ['hello', 'world', 'test', 'word', 'another test']
+numbers = [1, 2, 3, 4, 5]
+pool = ThreadPool(5)
+results = []
+for i in range(0, len(words)):
+    results.append(pool.apply_async(foo, args=(words[i], numbers[i])))
+
+pool.close()
+pool.join()
+results = [r.get() for r in results]
+print results
 
 
+# the queue module
 
+stdoutmutex = _thread.allocate_lock()
+numthreads = 5
+exitmutexes = [_thread.allocate_lock() for i in range(numthreads)]
+
+
+def counter(myId, count, mutex):
+    for i in range(count):
+        time.sleep(1/(myId + 1))
+        with mutex:
+            print('[%s] => %s' % (myId, i))
+    exitmutexes[myId].acquire()
+
+
+for i in range(numthreads):
+    _thread.start_new_thread(counter, (i, 5, stdoutmutex))
+
+while not all(mutex.locked() for mutex in exitmutexes):
+    time.sleep(0.25)
+print('Main thread exiting.')
